961713e15f0f5539af314b9dec2c38777551a322
misht <mishtigs@ucm.es>
2016-12-09 11:48:53 +0100

    Añadida nueva clase reset

---
+++ src/Reset.java
@@ -0,0 +1,22 @@
+
+public class Reset extends Command{
+
+	@Override
+	public boolean execute(Engine engine) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Command parse(String[] s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String textHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
