8a4647004951f1d2a6f992488a0834cafafc2fcb
misht <mishtigs@ucm.es>
2016-12-09 15:45:50 +0100

    Replace hecho

--- src/Replace.java
+++ src/Replace.java
@@ -1,26 +1,31 @@
 
 public class Replace extends Command {
-
+	private int replace;
+	
 	public Replace() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean execute(Engine engine) {
-		// TODO Auto-generated method stub
-		return false;
+		return engine.executeReplace(replace);
 	}
 
 	@Override
 	public Command parse(String[] s) {
-		// TODO Auto-generated method stub
-		return null;
+		if (s.length==1 && s[0].equalsIgnoreCase("Replace")) {
+			replace = Integer.parseInt(s[1]);
+			return new Replace();
+		}
+		else {
+			return null;
+		}
 	}
 
 	@Override
 	public String textHelp() {
-		// TODO Auto-generated method stub
-		return null;
+		return "REPLACE N: Replace insruction no. N with an instruction to be provided" +
+				System.getProperty("line.separator");
 	}
 
 }
