25054faa0db6e8b31fba4b3243d0a3f8996f900d
Raúl Torrijos <raultorr@ucm.es>
2016-12-11 15:14:33 +0100

    3

---
+++ src/Div.java
@@ -0,0 +1,22 @@
+
+public class Div extends ArithmeticOps {
+
+	@Override
+	public int operation(CPU cpu) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean execute(CPU cpu) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ByteCode parse(String[] words) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
